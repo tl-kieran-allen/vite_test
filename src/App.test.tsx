9898d855be8c2c8d55b3be00de57b99960255830
tl-kieran-allen <kieran.allen@truelayer.com>
2022-10-21 10:35:25 +0100

    allow testing of tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,14 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('App', () => {
-  it('should be truthy', () => {
-    expect(true).toBeTruthy()
-  })
-})
+describe("App", () => {
+  it("should be truthy", () => {
+    expect(true).toBeTruthy();
+  });
+
+  it("should render", () => {
+    render(<App />);
+    expect(screen.getByText("Vite + React")).toBeInTheDocument();
+  });
+});
