2f27a62f649393ae349bdfeddb454500c992eede
tl-kieran-allen <kieran.allen@truelayer.com>
2022-10-21 10:30:44 +0100

    initial test setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    include: ["./src/**/*.test.tsx", "./src/**/*.test.ts"],
+  },
+});
